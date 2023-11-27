93c7feea6d4af2d76eb137f0691f81456ff87a02
Given <moretigiven@gmail.com>
2023-11-27 14:16:05 +0200

    complete objects

--- Basics_JS/json.js
+++ Basics_JS/json.js
@@ -6,14 +6,48 @@
 //converting objects to json
 //object
 const post = {
-    id:1,
-    title:'post 1',
-    body:'body part'
+    id: 1,
+    title: 'post 1',
+    body: 'body part'
 };
-
+//format to be send to server
 const str = JSON.stringify(post);
 console.log(str)        //{"id":1,"title":"post 1","body":"body part"}
-//what would be send to server
 
+//we can also jsonify arrays e.g
+const posts = [
+    {
+        id: 1,
+        title: 'post 1',
+        body: 'body part 1'
+    },
+    {
+        id: 2,
+        title: 'post 2',
+        body: 'body part 2'
+    }
+];
+
+const str2 = JSON.stringify(posts);
+console.log(str2);          //[{"id":1,"title":"post 1","body":"body part 1"},{"id":2,"title":"post 2","body":"body part 2"}]
+
+
+
+
+
+
+//parse back to JSON
+const obj = JSON.parse(str);
+
+console.log(obj) //{ id: 1, title: 'post 1', body: 'body part' }
+
+const obj2 = JSON.parse(str2);
+console.log(obj2);
+/*
+[
+  { id: 1, title: 'post 1', body: 'body part 1' },
+  { id: 2, title: 'post 2', body: 'body part 2' }
+]
+*/
 
 
